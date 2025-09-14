f8df664ea10dc64025c1f488cec6b40ace9d3879
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2025-09-14 09:54:08 +0200

    simd

--- list_operations.ml
+++ list_operations.ml
@@ -12,16 +12,16 @@ let add_arrays_imperative a b c =
     c.(i) <- a.(i) +. b.(i)
   done
 
-module Float64x2 = Ocaml_simd.Float64x2
-module Float64x4 = Ocaml_simd.Float64x4
-module Int8x16 = Ocaml_simd.Int8x16
-module Int64x2 = Ocaml_simd.Int64x2
+module Float64x2 = Ocaml_simd_sse.Float64x2
+module Float64x4 = Ocaml_simd_sse.Float64x4
+module Int8x16 = Ocaml_simd_sse.Int8x16
+module Int64x2 = Ocaml_simd_sse.Int64x2
 
 let add_arrays_simd_f64x2 a b c =
   let n = Array.length a in
   let simd_size = 2 in
   let i = ref 0 in
-  
+
   while !i + simd_size <= n do
     let va = Float64x2.Float_array.get a ~idx:!i in
     let vb = Float64x2.Float_array.get b ~idx:!i in
@@ -29,7 +29,7 @@ let add_arrays_simd_f64x2 a b c =
     Float64x2.Float_array.set c ~idx:!i vc;
     i := !i + simd_size
   done;
-  
+
   for j = !i to n - 1 do
     c.(j) <- a.(j) +. b.(j)
   done
@@ -38,7 +38,7 @@ let add_arrays_simd_f64x4 a b c =
   let n = Array.length a in
   let simd_size = 4 in
   let i = ref 0 in
-  
+
   while !i + simd_size <= n do
     let va = Float64x4.Float_array.get a ~idx:!i in
     let vb = Float64x4.Float_array.get b ~idx:!i in
@@ -46,7 +46,7 @@ let add_arrays_simd_f64x4 a b c =
     Float64x4.Float_array.set c ~idx:!i vc;
     i := !i + simd_size
   done;
-  
+
   for j = !i to n - 1 do
     c.(j) <- a.(j) +. b.(j)
   done
@@ -77,12 +77,12 @@ let () =
   let b_simd = [| 8.0; 7.0; 6.0; 5.0; 4.0; 3.0; 2.0; 1.0 |] in
   let sum_simd2 = Array.create_float (Array.length a_simd) in
   let sum_simd4 = Array.create_float (Array.length a_simd) in
-  
+
   add_arrays_simd_f64x2 a_simd b_simd sum_simd2;
   Printf.printf "SIMD Float64x2 result: ";
   Array.iter (Printf.printf "%f ") sum_simd2;
   Printf.printf "\n";
-  
+
   add_arrays_simd_f64x4 a_simd b_simd sum_simd4;
   Printf.printf "SIMD Float64x4 result: ";
   Array.iter (Printf.printf "%f ") sum_simd4;
@@ -92,10 +92,9 @@ let () =
   let text = "abcdefghijklmnopqrstuvwxyz" in
   let floats = [| 1.0; 2.0 |] in
   let ints = [| 1; 2 |] in
-  
+
   let _ = Int8x16.String.get text ~byte:0 in
   let _ = Float64x2.Float_array.get floats ~idx:0 in
   let _ = Int64x2.Immediate_array.get_tagged ints ~idx:0 in
-  
-  Printf.printf "SIMD string/array access examples completed\n";
 
+  Printf.printf "SIMD string/array access examples completed\n"
